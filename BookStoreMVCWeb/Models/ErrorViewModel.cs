21e87ba24eda00be250552832962974ac9f10be2
Cipsour <cipcola2@gmail.com>
2024-12-23 15:27:11 +0700

    Revert "First upload"
    This reverts commit 414144cb1899d98fb300b3a60334e20c032ce40d.

--- BookStoreMVCWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BookStoreMVCWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- BookStoreMVCWeb/Models/ViewModel/BookDetailsViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookStoreMVCWeb.Models.ViewModel
-{
-	public class BookDetailsViewModel
-	{
-		public Book Book { get; set; }
-		public List<Review> Reviews { get; set; }
-	}
-}
